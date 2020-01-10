f7ea227f3a50fdb66eb2badc8fdf5f839f13c623
Courtney Jacobs <courtneyj3470@gmail.com>
2020-01-10 16:58:05 -0500

    Warriors API info
    entered and parsed
    Now lets add them to the functionality

--- NBAOraclePack.py
+++ NBAOraclePack.py
@@ -300,7 +300,47 @@ mavsGame = lastGameDateDAL + """
 #################################################################################
 # GOLDEN STATE WARRIORS
 
+# WARRIORS General Info 134865
+warriorsRE = requests.get("https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=Golden%20State%20Warriors")
 
+# WARRIORS Last Game 134865
+warriorsLG = requests.get("https://www.thesportsdb.com/api/v1/json/4013017/eventslast.php?id=134865")
+
+# WARRIORS Next Game 134865
+warriorsNG = requests.get("https://www.thesportsdb.com/api/v1/json/4013017/eventsnext.php?id=134865")
+
+# General Info Parsed
+dataGS = warriorsRE.text
+parseGS = json.loads(dataGS)
+# For the Test
+# print(warriorsRE.status_code)
+
+# Last Game Parsed
+LGdataGS = warriorsLG.text
+LGparseGS = json.loads(LGdataGS)
+
+# Next Game Parsed
+NGdataGS = warriorsNG.text
+NGparseGS = json.loads(NGdataGS)
+
+# info Layout for the Drop Down Menu to Gather from
+warriorsTeam = parseGS["teams"][0]["strTeam"]
+yearFormedGS = parseGS["teams"][0]["intFormedYear"]
+teamStadiumGS = parseGS["teams"][0]["strStadium"]
+teamInfoGS = parseGS["teams"][0]["strDescriptionEN"]
+lastGameDateGS = LGparseGS["results"][0]["dateEventLocal"]
+homeTeamGS = LGparseGS["results"][0]["strHomeTeam"]
+awayTeamGS = LGparseGS["results"][0]["strAwayTeam"]
+homeScoreGS = LGparseGS["results"][0]["intHomeScore"]
+awayScoreGS = LGparseGS["results"][0]["intAwayScore"]
+
+# Last Game Info Printout WARRIORS
+warriorsHome = "Home: " + str(homeTeamGS) + " " + str(homeScoreGS)
+warriorsAway = "Away: " + str(awayTeamGS) + " " + str(awayScoreGS)
+
+warriorsGame = lastGameDateGS + """
+""" + warriorsHome + """
+""" + warriorsAway + ""
 
 #################################################################################
 # DETROIT PISTONS
@@ -316,7 +356,7 @@ pistonsNG = requests.get("https://www.thesportsdb.com/api/v1/json/4013017/events
 dataDET = pistonsRE.text
 parseDET = json.loads(dataDET)
 # For the Test
-# print(pistonsDET.status_code)
+# print(pistonsRE.status_code)
 # print(json.dumps(parseDET, indent=4))
 
 # Last Game Parsed
