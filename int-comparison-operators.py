4b69382dc2e487ebae9500e867db316d702514b8
Courtney Jacobs <courtneyj3470@gmail.com>
2020-01-15 16:39:55 -0500

    int comparisons example
    example for int comparisons to determine wins and losses

---
+++ int-comparison-operators.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+a = 21
+b = 10
+c = 0
+
+if ( a == b ):
+   print "Line 1 - a is equal to b"
+else:
+   print "Line 1 - a is not equal to b"
+
+if ( a != b ):
+   print "Line 2 - a is not equal to b"
+else:
+   print "Line 2 - a is equal to b"
+
+if ( a <> b ):
+   print "Line 3 - a is not equal to b"
+else:
+   print "Line 3 - a is equal to b"
+
+if ( a < b ):
+   print "Line 4 - a is less than b" 
+else:
+   print "Line 4 - a is not less than b"
+
+if ( a > b ):
+   print "Line 5 - a is greater than b"
+else:
+   print "Line 5 - a is not greater than b"
