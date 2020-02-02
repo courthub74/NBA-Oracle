adcd9684f20189cf3205c0da004b09e3de87644e
Courtney Jacobs <courtneyj3470@gmail.com>
2020-02-02 18:43:10 -0500

    Boston Added

--- NBA Oracle/NBAOracle.py
+++ NBA Oracle/NBAOracle.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import teamslist
-import atl
+import atlanta
+import boston
 
 # WINDOW
 window = Tk()
@@ -20,43 +21,43 @@ logoNBA.grid(row=0,column=1,sticky=W)
 TeamsVar = StringVar() 
 TeamsVar.set(teamslist.TeamsList[0])
 droplist = OptionMenu(window, TeamsVar, *teamslist.TeamsList)
-droplist.config(width=20, height=1, fg="Darkblue")
+droplist.config(width=45, height=1, fg="Darkblue")
 droplist.grid(row=1,column=1,sticky=W)
 
 # TEAM SELECTED
 teamSelected = Label(window, text="Team Selected:", bg="Darkblue", fg="White")
 teamSelected.grid(row=2,column=0)
-teamSelectedOutput = Text(window, width=20, height=1)
+teamSelectedOutput = Text(window, width=40, height=1, bg="Black", fg="White")
 teamSelectedOutput.grid(row=2,column=1,sticky=W)
 
 # YEAR FORMED
 yearFormed = Label(window, text="Year Formed:", bg="Darkblue", fg="White")
 yearFormed.grid(row=3,column=0)
-yearFormedOutput = Text(window, width=20, height=1)
+yearFormedOutput = Text(window, width=40, height=1, bg="Black", fg="White")
 yearFormedOutput.grid(row=3,column=1,sticky=W)
 
 # TEAM STADIUM
 teamStadium = Label(window, text="Team Stadium:", bg="Darkblue", fg="White")
 teamStadium.grid(row=4,column=0)
-teamStadiumOutput = Text(window, width=20, height=1)
+teamStadiumOutput = Text(window, width=40, height=1, bg="Black", fg="White")
 teamStadiumOutput.grid(row=4,column=1,sticky=W)
 
 # TEAM INFO
 teamInfo = Label(window, text="Team Info:", bg="Darkblue", fg="White")
 teamInfo.grid(row=5,column=0)
-teamInfoOutput = Text(window, width=40, height=5, wrap=WORD)
+teamInfoOutput = Text(window, width=40, height=5, wrap=WORD, bg="Black", fg="White")
 teamInfoOutput.grid(row=5,column=1,sticky=W)
 
 # LAST GAME
 lastGame = Label(window, text="Last Game Played:", bg="Darkblue", fg="White")
 lastGame.grid(row=6,column=0)
-lastGameOutput = Text(window, width=30, height=4)
+lastGameOutput = Text(window, width=40, height=4, bg="Black", fg="White")
 lastGameOutput.grid(row=6,column=1,sticky=W)
 
 # WIN LOSS / STREAK
 outcome = Label(window, text="Win/Loss", bg="Darkblue", fg="White")
 outcome.grid(row=7,column=0)
-outcomeOutput = Text(window, width=20, height=1)
+outcomeOutput = Text(window, width=40, height=1, bg="Black", fg="White")
 outcomeOutput.grid(row=7,column=1,sticky=W)
 
 # LOGO
@@ -70,8 +71,12 @@ LogoCanvas.grid(row=8,column=1)
 def callTeamSelected(*args):
 	if TeamsVar.get() == "Atlanta Hawks":
 		teamSelectedOutput.delete(0.0, END)
-		selectATL = atl.hawksTeam
+		selectATL = atlanta.hawksTeam
 		teamSelectedOutput.insert(INSERT, selectATL)
+	if TeamsVar.get() == "Boston Celtics":
+		teamSelectedOutput.delete(0.0, END)
+		selectBOS = boston.celticsTeam
+		teamSelectedOutput.insert(INSERT, selectBOS)
 
 TeamsVar.trace("w", callTeamSelected)
 
@@ -79,8 +84,12 @@ TeamsVar.trace("w", callTeamSelected)
 def callYearFormed(*args):
 	if TeamsVar.get() == "Atlanta Hawks":
 		yearFormedOutput.delete(0.0, END)
-		yearATL = atl.yearFormedATL
+		yearATL = atlanta.yearFormedATL
 		yearFormedOutput.insert(INSERT, yearATL)
+	if TeamsVar.get() == "Boston Celtics":
+		yearFormedOutput.delete(0.0, END)
+		yearBOS = boston.yearFormedBOS
+		yearFormedOutput.insert(INSERT, yearBOS)
 
 TeamsVar.trace("w", callYearFormed)
 
@@ -88,8 +97,12 @@ TeamsVar.trace("w", callYearFormed)
 def callTeamStadium(*args):
 	if TeamsVar.get() == "Atlanta Hawks":
 		teamStadiumOutput.delete(0.0, END)
-		stadiumATL = atl.teamStadiumATL
+		stadiumATL = atlanta.teamStadiumATL
 		teamStadiumOutput.insert(INSERT, stadiumATL)
+	if TeamsVar.get() == "Boston Celtics":
+		teamStadiumOutput.delete(0.0, END)
+		stadiumBOS = boston.teamStadiumBOS
+		teamStadiumOutput.insert(INSERT, stadiumBOS)
 
 TeamsVar.trace("w", callTeamStadium)
 
@@ -97,8 +110,12 @@ TeamsVar.trace("w", callTeamStadium)
 def callTeamInfo(*args):
 	if TeamsVar.get() == "Atlanta Hawks":
 		teamInfoOutput.delete(0.0, END)
-		infoATL = atl.teamInfoATL
+		infoATL = atlanta.teamInfoATL
 		teamInfoOutput.insert(INSERT, infoATL)
+	if TeamsVar.get() == "Boston Celtics":
+		teamInfoOutput.delete(0.0, END)
+		infoBOS = boston.teamInfoBOS
+		teamInfoOutput.insert(INSERT, infoBOS)
 
 TeamsVar.trace("w", callTeamInfo)
 
@@ -106,8 +123,12 @@ TeamsVar.trace("w", callTeamInfo)
 def callLastGame(*args):
 	if TeamsVar.get() == "Atlanta Hawks":
 		lastGameOutput.delete(0.0, END)
-		hawksLastGame = atl.hawksGame 
+		hawksLastGame = atlanta.hawksGame 
 		lastGameOutput.insert(INSERT, hawksLastGame)
+	if TeamsVar.get() == "Boston Celtics":
+		lastGameOutput.delete(0.0, END)
+		celticsLastGame = boston.celticsGame 
+		lastGameOutput.insert(INSERT, celticsLastGame)
 
 TeamsVar.trace("w", callLastGame)
 
@@ -118,6 +139,11 @@ def callTeamLogo(*args):
 		hawksLogo = PhotoImage(file="logos/Hawks300.png")
 		LogoCanvas.image = hawksLogo
 		LogoCanvas.create_image(0, 0, anchor='nw', image=hawksLogo)
+	if TeamsVar.get() == "Boston Celtics":
+		LogoCanvas.delete("all")
+		celticsLogo = PhotoImage(file="logos/Celtics300.png")
+		LogoCanvas.image = celticsLogo
+		LogoCanvas.create_image(0, 0, anchor='nw', image=celticsLogo)
 
 TeamsVar.trace("w", callTeamLogo)
 
