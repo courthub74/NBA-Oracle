3c18d5378dc4b7ade758753cd35b3e61072ce093
Courtney Jacobs <courtneyj3470@gmail.com>
2020-02-02 14:47:07 -0500

    Update NBAOracle.py

--- NBA Oracle/NBAOracle.py
+++ NBA Oracle/NBAOracle.py
@@ -1,8 +1,6 @@
-from tkinter import *
-import teamslist
-
 # WINDOW
 window = Tk()
+window.geometry("500x800")
 window.iconbitmap("icon/NBAclear.ico")
 window.title("NBA Oracle")
 window.configure(background="Darkblue")
@@ -12,27 +10,53 @@ logo = PhotoImage(file="header/NBAclear.png")
 logoNBA = Label(window, image=logo, bg="Darkblue")
 resizeLogo = logo.subsample(1, 1)
 logoNBA.config(image=resizeLogo)
-logoNBA.grid(row=0,column=1)
+logoNBA.grid(row=0,column=1,sticky=W)
 
 # DROP DOWN MENU
 TeamsVar = StringVar() 
 TeamsVar.set(teamslist.TeamsList[0])
 droplist = OptionMenu(window, TeamsVar, *teamslist.TeamsList)
 droplist.config(width=30, height=1, fg="Darkblue")
-droplist.grid(row=1,column=1)
+droplist.grid(row=1,column=1,sticky=W)
 
 # TEAM SELECTED
 teamSelected = Label(window, text="Team Selected:", bg="Darkblue", fg="White")
 teamSelected.grid(row=2,column=0)
 teamSelectedOutput = Text(window, width=20, height=1)
-teamSelectedOutput.grid(row=2,column=1)
+teamSelectedOutput.grid(row=2,column=1,sticky=W)
 
 # YEAR FORMED
 yearFormed = Label(window, text="Year Formed:", bg="Darkblue", fg="White")
 yearFormed.grid(row=3,column=0)
 yearFormedOutput = Text(window, width=20, height=1)
-yearFormedOutput.grid(row=3,column=1)
+yearFormedOutput.grid(row=3,column=1,sticky=W)
 
+# TEAM STADIUM
+teamStadium = Label(window, text="Team Stadium:", bg="Darkblue", fg="White")
+teamStadium.grid(row=4,column=0)
+teamStadiumOutput = Text(window, width=20, height=1)
+teamStadiumOutput.grid(row=4,column=1,sticky=W)
 
+# TEAM INFO
+teamInfo = Label(window, text="Team Info:", bg="Darkblue", fg="White")
+teamInfo.grid(row=5,column=0)
+teamInfoOutput = Text(window, width=40, height=5)
+teamInfoOutput.grid(row=5,column=1,sticky=W)
+
+# LAST GAME
+lastGame = Label(window, text="Last Game Played:", bg="Darkblue", fg="White")
+lastGame.grid(row=6,column=0)
+lastGameOutput = Text(window, width=20, height=3)
+lastGameOutput.grid(row=6,column=1,sticky=W)
+
+# WIN LOSS / STREAK
+outcome = Label(window, text="Win/Loss", bg="Darkblue", fg="White")
+outcome.grid(row=7,column=0)
+outcomeOutput = Text(window, width=20, height=1)
+outcomeOutput.grid(row=7,column=1,sticky=W)
+
+# LOGO
+LogoCanvas = Canvas(height=300, width=300, borderwidth=0, highlightthickness=0, bg="Darkblue", bd=0)
+LogoCanvas.grid(row=8,column=1)
 
 window.mainloop()
